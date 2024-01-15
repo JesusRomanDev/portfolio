f94fbac76c48a799fefe5eeb0cf03ea93881b978
Jesus Roman <jesus_fime@outlook.es>
2024-01-15 14:38:10 -0600

    About Me edicion

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,21 +4,23 @@ import {useTypewriter, Cursor} from 'react-simple-typewriter'
 const AboutMe = () => {
     const [count, setCount] = useState(0)
     const [text] = useTypewriter({
-      words : ['Web Developer', 'Mechatronics Engineer', "Lifetime Learner"],
+      words : ['Front End Developer', 'Mechatronics Engineer', "Lifelong Learner"],
       loop: {},
       deleteSpeed: 80
     });
   return (
     <>
-        <section className='w-4/5 h-1/4 mx-auto flex gap-5 justify-center'>
-            <div className='w-1/2 flex flex-col justify-center items-center'>
-                <h1 className='text-white block'>Hi, my name is Jesus Eduardo</h1>
-                <h2 className='font-black text-white'>
+        <section className='mt-20 w-4/5 h-1/2 mx-auto flex gap-5 justify-center'>
+            <div className='w-1/2 flex flex-col justify-center'>
+                <h1 className='text-white text-left block text-4xl'>Hi, my name is <span className='font-bold text-red-800'>Jesus Eduardo</span></h1>
+                <h2 className='font-black text-white mt-5'>
                     I'm a<span className='text-red-700'> {text}</span><Cursor cursorColor='red' />
                 </h2>
+                <p className='text-white mt-5 font-semibold'>Based in <span className='text-red-500'>Monterrey Nuevo Leon, Mexico.</span> </p>
+                <p className='text-white font-semibold mt-5'>Graduated in Universidad Autonoma de Nuevo Leon as a Mechatronics Engineer, i'm a passionate person about the tech industry and everything that involves it, open to challenges, currently looking for my first job as a developer.</p>
             </div>
             <div className='w-1/4 flex flex-col justify-center items-center'>
-                <p className='text-center block'>seccion 2</p>
+                <img className='sliderUp' src="../../images/dev.png" alt="" />
             </div>
         </section>
     </>
