8d8a8648752510e9d39c2f64157e181748cd0367
Jesus Roman <jesus_fime@outlook.es>
2024-01-15 10:20:43 -0600

    Navbar edit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,11 +10,15 @@ const AboutMe = () => {
     });
   return (
     <>
-        <section>
-            <div>
-            <h2 className='font-black text-white'>
-                I'm a<span className='text-red-700'> {text}</span><Cursor cursorColor='red' />
-            </h2>
+        <section className='w-4/5 h-1/4 mx-auto flex gap-5 justify-center'>
+            <div className='w-1/2 flex flex-col justify-center items-center'>
+                <h1 className='text-white block'>Hi, my name is Jesus Eduardo</h1>
+                <h2 className='font-black text-white'>
+                    I'm a<span className='text-red-700'> {text}</span><Cursor cursorColor='red' />
+                </h2>
+            </div>
+            <div className='w-1/4 flex flex-col justify-center items-center'>
+                <p className='text-center block'>seccion 2</p>
             </div>
         </section>
     </>
