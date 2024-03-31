adb6c9db920503ea37166879e4484a922bc02c55
Jesus Roman <jesus_fime@outlook.es>
2024-03-30 18:23:47 -0600

    Cursor Pointer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,7 +32,7 @@ const Footer = () => {
                                 <path d="M12 16l0 -5" />
                                 <path d="M16 16v-3a2 2 0 0 0 -4 0" />
                             </svg></a>
-                            <div className='flex items-center border-2 rounded'><p className='email px-2'>jesus.romanv@outlook.com</p><svg onClick={copy} className='cursor-pointer' xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy" width="22" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <div className='flex items-center border-2 rounded'><p className='cursor-pointer px-2'>jesus.romanv@outlook.com</p><svg onClick={copy} className='cursor-pointer' xmlns="http://www.w3.org/2000/svg"  width="22" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
                             <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
