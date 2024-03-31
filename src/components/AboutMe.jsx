7a58ef312644c92c95b67d692e91e07f2442d028
Jesus Roman <jesus_fime@outlook.es>
2024-03-30 19:12:33 -0600

    CV para descargar

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -24,7 +24,9 @@ const AboutMe = () => {
             <div className='sm:w-1/4 flex flex-col justify-start sm:justify-center items-center'>
                 <img className='w-3/4 sm:w-full sliderUp' src="img/dev.png" alt="dev" />
                 <div>
-                    <button className='hover:bg-slate-400 hover:scale-110 hover:duration-300 ease-out duration-300 text-xl text-white font-semibold p-2 bg-slate-600 rounded-md'>Download CV</button>       
+                    <a download='EduardoRomanCV' href='../../cv.pdf'>
+                    <button className='hover:bg-slate-400 hover:scale-110 hover:duration-300 ease-out duration-300 text-xl text-white font-semibold p-2 bg-slate-600 rounded-md'>Download CV</button>
+                    </a>       
                 </div>
             </div>
         </section>
