34e79cf55919d4e621cf4f7d57fa8b5be6a3b4fa
Jesus Roman <jesus_fime@outlook.es>
2024-03-30 18:43:20 -0600

    Hover en copiar portapapeles y alerta

---
+++ src/components/Alerta.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Alerta = () => {
+  return (
+    <div className='text-green-500 absolute'>Copiado!</div>
+  )
+}
+
+export default Alerta
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Alerta from './Alerta';
 
 const Footer = () => {
+    const [copiado, setCopiado] = useState(false);
     const fecha = new Date().getFullYear();
     const copy = () => {
         navigator.clipboard
         .writeText('jesus.romanv@outlook.com');
+        setCopiado(true);
+        setTimeout(() => {
+            setCopiado(false);
+        }, 2500);
     }
   return (
     <>
@@ -18,6 +24,7 @@ const Footer = () => {
                     <div className='flex-col gap-5 font-bold text-white'>
                         <div>
                             <h2>Contact Me</h2>
+                            {copiado && <Alerta />}
                         </div>
                         <div className='flex items-center justify-center gap-5 mt-5'>
                             <a aria-label='GitHub' target='_blank' href="https://github.com/JesusRomanDev"><svg xmlns="http://www.w3.org/2000/svg" className="hover:scale-125 hover:duration-300 ease-out duration-300 icon icon-tabler icon-tabler-brand-github" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ece0e0" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -32,11 +39,19 @@ const Footer = () => {
                                 <path d="M12 16l0 -5" />
                                 <path d="M16 16v-3a2 2 0 0 0 -4 0" />
                             </svg></a>
-                            <div className='flex items-center border-2 rounded'><p className='cursor-pointer px-2'>jesus.romanv@outlook.com</p><svg onClick={copy} className='cursor-pointer' xmlns="http://www.w3.org/2000/svg"  width="22" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                            <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-                            <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
-                            </svg></div>
+                            <div className='flex items-center border-2 rounded'>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="52" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+                                <path d="M3 7l9 6l9 -6" />
+                                </svg>
+                                <p className='px-2'>jesus.romanv@outlook.com</p>
+                                <svg onClick={copy} className='cursor-pointer' xmlns="http://www.w3.org/2000/svg"  width="22"   height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                    <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+                                    <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
+                                </svg>
+                            </div>
                         </div>
                     </div>
                 </div>
